52ae05170209f64a2f16ff1d64cce0b3b6a8fae7
Oleg Ignatenko <olegignatenko94@gmail.com>
2020-04-16 13:23:50 +0300

    Test

--- src/test/java/com/qatestlab/testcase/TestCase.java
+++ src/test/java/com/qatestlab/testcase/TestCase.java
@@ -9,24 +9,40 @@ import static org.testng.Assert.assertTrue;
 public class TestCase extends CheckPage {
 
     @Test
-    public void testAll() {
+    public void testUsd() {
         changeCurrency(Currency.USD);
         assertTrue(checkCurrency(Currency.USD));
+    }
+
+    @Test
+    public void testEur() {
 
         changeCurrency(Currency.EUR);
         assertTrue(checkCurrency(Currency.EUR));
+    }
 
+    @Test
+    public void testUah(){
         changeCurrency(Currency.UAH);
         assertTrue(checkCurrency(Currency.UAH));
+    }
 
+    @Test
+    public void testSearch(){
         changeCurrency(Currency.USD);
         searchByWord();
         assertTrue(checkCountFoundedProducts());
         assertTrue(checkCurrencyFoundedProducts(Currency.USD));
+    }
 
+    @Test
+    public void testSort(){
         setSortingFromHighToLow();
         assertTrue(checkSortingWithoutDiscount());
+    }
 
+    @Test
+    public void testDiscount(){
         assertTrue(checkDiscount());
     }
 }
